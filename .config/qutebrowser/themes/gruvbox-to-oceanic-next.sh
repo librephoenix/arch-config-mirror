4bad352720a496edfd2fd4f7efe4f7b609243959
Emmet <librephoenix3@pm.me>
2022-05-28 12:10:07 -0500

    Added custom qutebrowser css (gruvbox+oceanicnext)

---
+++ .config/qutebrowser/themes/gruvbox-to-oceanic-next.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cp gruvbox-all-sites.css oceanic-next-all-sites.css
+sed -i "s/#fabd2f/#fac863/g" oceanic-next-all-sites.css
+sed -i "s/#458588/#6699cc/g" oceanic-next-all-sites.css
+sed -i "s/#d3869b/#c594c5/g" oceanic-next-all-sites.css
+sed -i "s/#83a598/#4477ef/g" oceanic-next-all-sites.css
+sed -i "s/#282828/#1b2b34/g" oceanic-next-all-sites.css
+sed -i "s/#2c2c2c/#1b2c35/g" oceanic-next-all-sites.css
+sed -i "s/#3c3836/#20414f/g" oceanic-next-all-sites.css
+sed -i "s/#2f1f1d/#131f25/g" oceanic-next-all-sites.css
+sed -i "s/#232323/#101a1f/g" oceanic-next-all-sites.css
+sed -i "s/#a89984/#65737e/g" oceanic-next-all-sites.css
+sed -i "s/#b8bb26/#77f774/g" oceanic-next-all-sites.css
+sed -i "s/#8ec07c/#4fa3a3/g" oceanic-next-all-sites.css
+sed -i "s/#403c3a/#405860/g" oceanic-next-all-sites.css
+sed -i "s/#98971a/#99c794/g" oceanic-next-all-sites.css
+sed -i "s/#cc241d/#ec5f67/g" oceanic-next-all-sites.css
+sed -i "s/rgba(231,230,113/rgba(231,212,114/g" oceanic-next-all-sites.css
+sed -i "s/#727113/#5d8257/g" oceanic-next-all-sites.css
+sed -i "s/#c1d2cb/#c2cdd2/g" oceanic-next-all-sites.css
+sed -i "s/rgba(204,36,29/rgba(236,95,103/g" oceanic-next-all-sites.css
+sed -i "s/#66120f/#7d3237/g" oceanic-next-all-sites.css
+sed -i "s/#689d6a/#5fb3b3/g" oceanic-next-all-sites.css
+sed -i "s/#44403e/#101a1f/g" oceanic-next-all-sites.css
+sed -i "s/#d79921/#fac863/g" oceanic-next-all-sites.css
+sed -i "s/rgba(185,255,255/rgba(187,233,255/g" oceanic-next-all-sites.css
+sed -i "s/rgba(255,34,23/rgba(236,93,100/g" oceanic-next-all-sites.css
+sed -i "s/rgba(212,211,18/rgba(128,199,123/g" oceanic-next-all-sites.css
