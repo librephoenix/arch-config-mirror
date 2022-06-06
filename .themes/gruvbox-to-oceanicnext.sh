3171883a7234091442fdc4e36efd1abeb20a261f
Emmet <librephoenix3@pm.me>
2022-06-06 16:06:22 -0500

    Added my ~/.themes directory for custom GTK

---
+++ .themes/gruvbox-to-oceanicnext.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#1b2b34/#1b2b34/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#81831b/#3a6435/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#93961e/#43753e/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#a6a822/#4d8646/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#b8bb26/#60a758/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#c1c428/#90c18a/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#cace2a/#92c58f/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#d5c4a1/#9dacc8/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#ebdbb2/#d8dee9/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#fbf1c7/#eff1f6/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#83a598/#6699cc/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/#342a17/#111c22/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/rgba(178, 147, 83,/rgba(85, 166, 178,/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/rgba(184, 187, 38,/rgba(153, 199, 148,/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/rgba(190, 164, 109,/rgba(108, 190, 190,/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/rgba(196, 172, 122,/rgba(122, 196, 163,/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/rgba(213, 196, 161,/rgba(165, 213, 203,/g" {}
+fd . ~/.themes/MyOceanicNext/ --type f --exec sed -i "s/rgba(52, 42, 23,/rgba(23, 43, 52,/g" {}
