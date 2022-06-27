521efc41ee6b4869444ffda907097865a3723cca
Emmet <librephoenix3@pm.me>
2022-06-27 11:51:24 -0500

    Dual monitor setup script for both {lap,desk}top

--- .local/bin/setup_external_monitor.sh
+++ .local/bin/setup_external_monitor.sh
@@ -6,11 +6,26 @@
 xrandr_command="/usr/bin/xrandr"
 sed_command="/bin/sed"
 
-is_hdmi_connected=`DISPLAY=:0 $xrandr_command | $sed_command -n '/HDMI1 connected/p'`
+# For my laptop
+if [ $(hostname) = "dusty" ]; then
+	is_hdmi_connected=`DISPLAY=:0 $xrandr_command | $sed_command -n '/HDMI1 connected/p'`
 
-if [ -n "$is_hdmi_connected" ]; then
-	  DISPLAY=:0 $xrandr_command --output HDMI1 --auto --right-of eDP1 
-  else
-	    DISPLAY=:0 $xrandr_command --output HDMI1 --off
+	if [ -n "$is_hdmi_connected" ]; then
+		  DISPLAY=:0 $xrandr_command --output HDMI1 --auto --right-of eDP1 
+		    else
+                  DISPLAY=:0 $xrandr_command --output HDMI1 --off
+	fi
 fi
+
+# For my desktop
+if [ $(hostname) = "rusty" ]; then
+	is_dp_0_connected=`DISPLAY=:0 $xrandr_command | $sed_command -n '/DP-0 connected/p'`
+
+	if [ -n "$is_dp_0_connected" ]; then
+		  DISPLAY=:0 $xrandr_command --output DP-0 --auto --right-of DVI-I-1
+  	else
+                  DISPLAY=:0 $xrandr_command --output DP-0 --off
+	fi
+fi
+
 nitrogen --restore
