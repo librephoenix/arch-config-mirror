800a0a2075e70d3a2e0298a4f1b3257e2a57ebe3
Emmet <librephoenix3@pm.me>
2023-02-16 11:26:07 -0600

    Debloated external monitor setup script

--- .local/bin/setup-external-monitor.sh
+++ .local/bin/setup-external-monitor.sh
@@ -5,17 +5,12 @@
 
 xrandr_command="/usr/bin/xrandr"
 sed_command="/bin/sed"
-
-# For my laptop
-if [ $(hostname) = "ordinal" ]; then
-	is_hdmi_connected=`DISPLAY=:0 $xrandr_command | $sed_command -n '/HDMI-A-0 connected/p'`
-
-	if [ -n "$is_hdmi_connected" ]; then
-		  DISPLAY=:0 $xrandr_command --dpi 120 --output HDMI-A-0 --auto --pos 1920x0 --output eDP --auto --pos 1000x1200 --output DisplayPort-1-0 --auto --pos 0x0
-		    else
-                  DISPLAY=:0 $xrandr_command --dpi 120 --output HDMI-A-0 --off --output Display-Port-1-0 --off --output eDP --auto
+is_hdmi_connected=`DISPLAY=:0 $xrandr_command | $sed_command -n '/HDMI-A-0 connected/p'`
+ if [ -n "$is_hdmi_connected" ]; then
+	  DISPLAY=:0 $xrandr_command --dpi 120 --output HDMI-A-0 --auto --pos 1920x0 --output eDP --auto --pos 1000x1200 --output DisplayPort-1-0 --auto --pos 0x0
+	    else
+         DISPLAY=:0 $xrandr_command --dpi 120 --output HDMI-A-0 --off --output Display-Port-1-0 --off --output eDP --auto
 	fi
-fi
 
 nitrogen --restore
 xmonad --restart
