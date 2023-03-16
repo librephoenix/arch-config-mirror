1e5b95cb47ab71d276f94dbc7645b4150313a5e9
Emmet <librephoenix3@pm.me>
2023-03-16 16:39:12 -0500

    Fixed centaur tab autohiding

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -921,36 +921,8 @@ https://github.com/magit/magit/issues/460 (@cpitclaudel)."
   "H" #'centaur-tabs-backward
   "L" #'centaur-tabs-forward)
 
-(defun centaur-tabs-hide-tab (x)
-  "Do no to show buffer X in tabs."
-  (let ((name (format "%s" x)))
-    (or
-     ;; Current window is not dedicated window.
-     (window-dedicated-p (selected-window))
-
-     ;; Buffer name not match below blacklist.
-     (string-prefix-p "*epc" name)
-     (string-prefix-p "*helm" name)
-     (string-prefix-p "*Helm" name)
-     (string-prefix-p "*Compile-Log*" name)
-     (string-prefix-p "*compilation*" name)
-     (string-prefix-p "*Ibuffer*" name)
-     (string-prefix-p "*Messages*" name)
-     (string-prefix-p "*lsp" name)
-     (string-prefix-p "*company" name)
-     (string-prefix-p "*Flycheck" name)
-     (string-prefix-p "*tramp" name)
-     (string-prefix-p " *Mini" name)
-     (string-prefix-p "*help" name)
-     (string-prefix-p "*straight" name)
-     (string-prefix-p " *temp" name)
-     (string-prefix-p "*Help" name)
-     (string-prefix-p "*mybuf" name)
-
-     ;; Is not magit buffer.
-     (and (string-prefix-p "magit" name)
-          (not (file-name-extension name)))
-     )))
+(setq centaur-tabs-excluded-prefixes
+  '("*scratch" "*elfeed-log" "*Messages" "*Ibuffer" "*Native-compile-log" "*Async-native-compile-log" "*epc" "*helm" "*Helm" " *which" "*Compile-Log*" "*lsp" "*LSP" "*company" "*Flycheck" "*Ediff" "*ediff" "*tramp" " *Mini" "*help" "*straight" " *temp" "*Help"))
 
 ;;;------ Load my private config ------;;;
 
